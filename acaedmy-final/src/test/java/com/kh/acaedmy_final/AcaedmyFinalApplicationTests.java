3f50c993b71c527fa90b449f1ffcce7510d2b565
sanghunlee1333 <sanghunlee1333@gmail.com>
2025-03-25 15:33:01 +0900

    최초 프로젝트 생성 및 초기설정

---
+++ acaedmy-final/src/test/java/com/kh/acaedmy_final/AcaedmyFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.acaedmy_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaedmyFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
