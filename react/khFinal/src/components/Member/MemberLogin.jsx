d9d1a215db2100c88c3c1dafc70ced65531d9d51
pyojaesohnkh15 <pjs.kh15@gmail.com>
2025-04-23 14:21:24 +0900

    Route 자리 생성

--- react/khFinal/src/components/Member/MemberLogin.jsx
+++ react/khFinal/src/components/Member/MemberLogin.jsx
@@ -1,4 +1,21 @@
-export default function MemberLogin(){
+import { useState } from "react";
 
-    return (<></>);
+export default function MemberLogin(){
+    //state
+    const [members, setMembers] = useState([]);
+    const [input, setInput] = useState("");
+
+
+
+
+    // view
+    return (<>
+    <div className="row mt-4">
+        <label className="col-sm-3 form-label-control"></label>
+        <div className="col-sm-9">
+            <input type="text" className="form-control"/>
+        </div>
+    </div>
+    
+    </>);
 }
