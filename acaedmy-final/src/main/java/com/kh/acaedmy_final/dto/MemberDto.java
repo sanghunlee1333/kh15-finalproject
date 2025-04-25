41ae2b5d2220a6cc2bfb5de98176930ad25007cc
pyojaesohnkh15 <pjs.kh15@gmail.com>
2025-04-25 16:58:44 +0900

    member

--- acaedmy-final/src/main/java/com/kh/acaedmy_final/dto/MemberDto.java
+++ acaedmy-final/src/main/java/com/kh/acaedmy_final/dto/MemberDto.java
@@ -11,4 +11,16 @@ public class MemberDto {
 	private String memberId;
 	private String memberPw;
 	private String memberDepartment;
+	private String memberName;
+	private String memberResidentNo;
+	private String memberContact;
+	private String memberEmail;
+	private String memberPost;
+	private String memberAddress1;
+	private String memberAddress2;
+	private String memberRank;
+	private String memberBank;
+	private String memberBankNo;
+	private String memberJoin;
+	private String memberState;
 }
