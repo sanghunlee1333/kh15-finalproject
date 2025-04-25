ce47fd73f86d25de2f2940bbaed5c8827bbd4390
ingttaetest <ingttaekh@gmail.com>
2025-04-25 20:47:03 +0900

    연락처 전체 회원 목록 기능 구현

--- react/khFinal/src/App.jsx
+++ react/khFinal/src/App.jsx
@@ -7,10 +7,10 @@ import Sidebar from './components/template/Sidebar'
 import MemberLogin from './components/Member/MemberLogin'
 import NoticeList from './components/Notice/NoticeList'
 import NoticeDetail from './components/Notice/NoticeDetail'
-import ChatContact from './components/Websocket/ChatContact'
 import GroupChat from './components/Websocket/GroupChat'
 import ChatRoom from './components/Websocket/ChatRoom'
 import MemberJoin from './components/Member/MemberJoin'
+import MemberContact from './components/Member/MemberContact'
 
 function App() {
 
@@ -37,8 +37,10 @@ function App() {
           <Route path="/notice/detail" element={<NoticeDetail/>}></Route>
           <Route path="/notice/write" element={<MemberLogin/>}></Route>
           
-          {/* ChatContact */}
-          <Route path="/chat/contact" element={<ChatContact/>}></Route>
+          {/* Contact */}
+          <Route path="/member/contact" element={<MemberContact/>}></Route>
+
+          {/* Chat */}
           <Route path="/chat/room" element={<ChatRoom/>}></Route>
           <Route path="/chat/group" element={<GroupChat/>}></Route>
         </Routes>
