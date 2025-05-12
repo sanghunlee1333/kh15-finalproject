6b1338185a8070603c6e62892b7cc868b2f8df87
sanghunlee1333 <sanghunlee1333@gmail.com>
2025-05-12 09:45:28 +0900

    Todo 리스트 구현 중

--- react/khFinal/src/App.jsx
+++ react/khFinal/src/App.jsx
@@ -24,6 +24,7 @@ import { Bounce, ToastContainer } from 'react-toastify'
 import NoticeEdit from './components/Notice/NoticeEdit'
 import TeamPlan from './components/Plan/TeamPlan'
 import DateMange from './components/Admin/DateManage'
+import TodoList from './components/Plan/TodoList'
 
 function App() {
   const [userNo, setUserNo] = useRecoilState(userNoState);
@@ -101,6 +102,7 @@ function App() {
           
           {/* Plan */}
           <Route path="/plan/team" element={<TeamPlan/>}></Route>
+          <Route path="/plan/todo" element={<TodoList/>}></Route>
 
           {/* Contact */}
           <Route path="/member/contact" element={<MemberContact/>}></Route>
